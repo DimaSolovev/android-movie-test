a094351931184cc8e1012c95b2fa8a59c231017b
Dima <soloveid87@mail.ru>
2022-08-31 13:09:47 +1000

    detail activiti trailers and revies

---
+++ app/src/main/java/com/dima/myapplication/data/Review.java
@@ -0,0 +1,14 @@
+package com.dima.myapplication.data;
+
+public class Review {
+
+    private String author;
+    private String content;
+
+    public Review(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+
+
+}
--- app/src/main/java/com/dima/myapplication/util/JSONUtil.java
+++ app/src/main/java/com/dima/myapplication/util/JSONUtil.java
@@ -1,6 +1,10 @@
 package com.dima.myapplication.util;
 
+import androidx.dynamicanimation.animation.SpringAnimation;
+
 import com.dima.myapplication.data.Movie;
+import com.dima.myapplication.data.Review;
+import com.dima.myapplication.data.Trailer;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,11 +15,20 @@ import java.util.List;
 
 public class JSONUtil {
 
+    private static final String KEY_RESULTS = "results";
+    //отзывы
+    private static final String KEY_AUTHOR = "author";
+    private static final String KEY_CONTENT = "content";
+    //видео
+    private static final String KEY_KEY_OF_VIDEO = "key";
+    private static final String KEY_NAME = "name";
+    private static final String BASE_YOUTUBE_URL = "https://www.youtube.com/watch?v=";
+
     private static final String BASE_POSTER_URL = "https://image.tmdb.org/t/p/";
     private static final String SMALL_POSTER_SIZE = "w185";
     private static final String BIG_POSTER_SIZE = "w780";
 
-    private static final String KEY_RESULTS = "results";
+
     private static final String KEY_ID = "id";
     private static final String KEY_VOTE_COUNT = "vote_count";
     private static final String KEY_TITLE = "title";
@@ -25,6 +38,48 @@ public class JSONUtil {
     private static final String KEY_BACKGROUND_PATH = "backdrop_path";
     private static final String KEY_VOTE_AVERAGE = "vote_average";
     private static final String KEY_RELEASE_DATE = "release_date";
+
+    public static List<Trailer> getTrailersFromJSON(JSONObject jsonObject) {
+        List<Trailer> result = new ArrayList<>();
+        if (jsonObject == null) {
+            return result;
+        }
+
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray(KEY_RESULTS);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObjectTrailer = jsonArray.getJSONObject(i);
+                String key = BASE_YOUTUBE_URL + jsonObjectTrailer.getString(KEY_KEY_OF_VIDEO);
+                String name = jsonObjectTrailer.getString(KEY_NAME);
+                Trailer trailer = new Trailer(key, name);
+                result.add(trailer);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static List<Review> getReviewsFromJSON(JSONObject jsonObject) {
+        List<Review> result = new ArrayList<>();
+        if (jsonObject == null) {
+            return result;
+        }
+
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray(KEY_RESULTS);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObjectReview = jsonArray.getJSONObject(i);
+                String author = jsonObjectReview.getString(KEY_AUTHOR);
+                String content = jsonObjectReview.getString(KEY_CONTENT);
+                Review review = new Review(author, content);
+                result.add(review);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
     public static List<Movie> getMoviesFromJSON(JSONObject jsonObject) {
         List<Movie> result = new ArrayList<>();
